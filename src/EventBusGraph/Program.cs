bd9b52c240f92f1ac5e347a3958b652ef7b0cf28
Jonas Askeland <jonask@attensi.com>
2022-03-25 10:12:23 +0100

    Add commandline parsing

--- src/EventBusGraph/Program.cs
+++ src/EventBusGraph/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.CommandLine;
+
+var argument = new Argument<string>("Solution", "Path to the solution file in your unity projects root folder");
+var outputOption = new Option<string?>("--out", "Path to put the generated json into");
+outputOption.AddAlias("-o");
+var cmd = new RootCommand
+{
+    argument,
+    outputOption
+};
+
+cmd.SetHandler((string pathToSolution, string? pathToOutput) =>
+{
+    Console.WriteLine(pathToSolution);
+    Console.WriteLine(pathToOutput);
+}, argument, outputOption);
+
+cmd.Invoke(args);
